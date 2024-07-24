555089e3a826630953e1d33feb1c80c8fbbbbe8c
caitriona-brennan <161471100+caitriona-brennan@users.noreply.github.com>
2024-07-24 21:00:07 +0100

    update deploy repo

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ sudo npm install -g pm2
 # stop any instance of our application running currently
 pm2 stop example_app
 # change directory into folder where application is downloaded
-cd ExampleApplication/
+cd DevOpsProject/
 # Install application dependencies
 npm install
 # Start the application with the process name example_app using pm2
